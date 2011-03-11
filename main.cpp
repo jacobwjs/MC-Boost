c7a49dc0d4cf221140410aa0a9099d1f781fa45c
jacobwjs <jacobwjs@gmail.com>
2011-03-11 11:02:49 +0100

    adding detection of CPU architecture to launch number of threads equal to number of cores.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
 
 using namespace std;
 
-const int MAX_PHOTONS = 100000;
+const int MAX_PHOTONS = 2500000;
 
 //#define DEBUG 1
 
@@ -30,7 +30,7 @@ const int MAX_PHOTONS = 100000;
 int main()
 {
 	
-	
+
 	// Create the medium in which the photons will be fired.
 	Medium *tissue = new Medium();
 	
@@ -38,50 +38,58 @@ int main()
 	// handled in the 'tissue' object.
 	tissue->addLayer(new Layer());
 	
-	// Create a photon object, which will be propogated through the medium
-	// MAX_PHOTON times.
-	Photon *photon = new Photon();
-	Photon *photon2 = new Photon();
 	
 	// Allocate the planar grid and set it in the tissue.
 	double *Cplanar = (double*)malloc(sizeof(double) * 101);
 	tissue->setPlanarArray(Cplanar);
 		
+	// Notify user of execution.
+	cout << "Launching photons...\n";
+
 	// Capture the time before launching photons into the medium.
 	clock_t start, end;
 	start = clock();
 	
+
 	// Let boost decide how many threads to run on this architecture.
-	int NUM_THREADS = boost::thread::hardware_concurrency();
+	const int NUM_THREADS = boost::thread::hardware_concurrency();
 	
-	boost::thread thread1(&Photon::injectPhoton, photon, tissue, MAX_PHOTONS/2);
-	boost::thread thread2(&Photon::injectPhoton, photon2, tissue, MAX_PHOTONS/2);
-	thread1.join();
-	thread2.join();
-    
-	
-	
-	// Non-threaded case.
-	//photon->injectPhoton(tissue, MAX_PHOTONS);
-	
-	
+	// Each thread needs it's own photon object to run, so we need to create
+	// an equal amount of photon objects as threads.
+	const int NUM_PHOTON_OBJECTS = NUM_THREADS;
+
+	Photon photons[NUM_PHOTON_OBJECTS];
+	boost::thread threads[NUM_THREADS];
+
+	// Create the threads and given them photon objects to run.
+	// Each photon object is run MAX_PHOTONS/NUM_THREADS times, which essentially
+	// splits up the work (i.e. photon propagation) amongst many workers.
+	for (int i = 0; i < NUM_PHOTON_OBJECTS; i++)
+	{
+		cout << "Launching photon object" << i << " iterations: " << MAX_PHOTONS/NUM_THREADS << endl;
+		threads[i] = boost::thread(&Photon::injectPhoton, &photons[i], tissue, MAX_PHOTONS/NUM_THREADS);
+
+		//threads[i].thread(&Photon::injectPhoton, photons[i], tissue, MAX_PHOTONS/NUM_THREADS);
+		//photons[i].injectPhoton(tissue, MAX_PHOTONS/3);
+	}
+
+	// Join all created threads once they have done their work.
+	for (int i = 0; i < NUM_PHOTON_OBJECTS; i++)
+	{
+		threads[i].join();
+	}
+
 	
 	// Print out the elapsed time it took from beginning to end.
 	end = ((double)clock() - start) / CLOCKS_PER_SEC;
 	cout << "Time elapsed: " << end << endl;
 	
-	
-	
-	
 	// Print the matrix of the photon absorptions to file.
 	tissue->printGrid(MAX_PHOTONS);
 	
-	
 	// Clean up memory allocated memory on the heap.
 	delete tissue;
-	delete photon;
+	//delete photon;
 	
 	return 0;
 }
-
-
